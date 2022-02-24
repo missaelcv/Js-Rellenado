03e76a62b534d466322392bd37c91386246652d9
missaelcv <missaelcv0220@gmail.com>
2022-02-23 23:13:34 -0400

    Juego de operadores

--- Js-Basico/if.js
+++ Js-Basico/if.js
@@ -13,3 +13,32 @@ if (edad === 18 ) {
 }
 
 condition ? true: false;
+
+
+var numero = 3; 
+
+var resultado = numero === 1 ? "Soy un numero 1" : "no Soy el uno";
+
+console.log(resultado);
+
+
+var tiro1 = "papel";
+var tiro2 = "tijera";
+var tiro3 = "piedra";
+
+var juego = function(usuario, maquina){
+    if (usuario != maquina){
+        if (usuario === tiro1 && maquina === tiro3) {
+            console.log("el usuario Gano con"+ tiro1)
+        } else if (usuario === tiro2 && maquina === tiro1){
+            console.log("el usuario Gano con"+ tiro2)
+        } else if (usuario === tiro3 && maquina === tiro2){
+            console.log("el usuario Gano con"+ tiro3)
+    } else { 
+        console.log("Gano el Usuario");
+    }
+        } else if (usuario === maquina){
+            console.log("Emapte")
+        }
+}
+juego(tiro1,tiro3);
