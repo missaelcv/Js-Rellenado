6c7d7b062ea463123404cdfa4e4f6df80b265b6e
missaelcv <missaelcv0220@gmail.com>
2022-02-23 22:57:45 -0400

    creando operadores

---
+++ Js-Basico/if.js
@@ -0,0 +1,15 @@
+//Operadores 
+
+var edad = 18;
+
+if (edad === 18 ) {
+    console.log("Puedes Votar...");
+} else if (edad > 19 ) {
+    console.log("Tu segunda Votacion ")
+} else if (edad > 20 ) {
+    console.log("Tu segunda Votacion ")
+} else {
+    console.log("Aun no puedes votar ")
+}
+
+condition ? true: false;
