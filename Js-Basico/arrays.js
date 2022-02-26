d678cea5c4092665b6d38b5c2ec43078a31d7cc0
missaelcv <missaelcv0220@gmail.com>
2022-02-25 20:54:17 -0400

    agrendo metodo arrays

---
+++ Js-Basico/arrays.js
@@ -0,0 +1,24 @@
+// iniciar un arrray
+var frutas = ["Mango","Pera","Guineo","Cereza"];
+
+console.log(frutas);
+
+//para ver la longitud del arreglo.
+
+console.log(frutas.length);
+
+//Ver el arreglo o position designada
+
+console.log(frutas[2]);
+
+//Para agregar un objeto nuevo con el método Push 
+
+var masFrutas = frutas.push("uvas");
+
+//Eliminar un arrays con el método pop
+
+var ultimo = frutas.pop("uvas");
+
+//Como Agregar un objeto con el método unshift para un arrays 
+
+var nuevaFruta = frutas.unshift("guineo");
