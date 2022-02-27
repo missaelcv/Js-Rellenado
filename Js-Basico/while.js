6b6894da1db06c7e9b3491917e4f45db48098900
missaelcv <missaelcv0220@gmail.com>
2022-02-26 21:46:00 -0400

    ciclo while

---
+++ Js-Basico/while.js
@@ -0,0 +1,12 @@
+var estudiantes  = ["Maria","Sergio","Missael", "Maileny"];
+
+function saludarEstudiantes(estudiante){
+    console.log(`Hola, ${estudiante}`);
+}
+
+//Genera una variable 
+while(estudiantes.length > 0){
+    console.log(estudiantes);
+    var estudiante = estudiantes.shift();
+    saludarEstudiantes(estudiante);
+}
