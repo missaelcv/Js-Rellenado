4032bf0e0838d66cf4c5f3a3d01da24755e65e1c
missaelcv <missaelcv0220@gmail.com>
2022-02-26 21:59:02 -0400

    Objetos metodos this

---
+++ Js-Basico/Objetos.js
@@ -0,0 +1,11 @@
+var miCarro = {
+
+    //primera propiedad con valor
+        marca : "Honda",
+        modelo : "Civil",
+        annio : "2022",
+        detalleDelCarro : function(){
+            console.log(`Auto ${this.modelo} ${this.annio}`);
+        }
+} ; 
+
