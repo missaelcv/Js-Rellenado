cd536e48763f77983c1e7836eec9077999b2af8e
missaelcv <missaelcv0220@gmail.com>
2022-02-27 22:21:06 -0400

    metodos de arrays

---
+++ Js-Basico/MetodosArrays.js
@@ -0,0 +1,10 @@
+var articulos = [
+    
+    {nombre: "Bicicleta",  costo: 3000},
+    {nombre: "Television", costo:2500},
+    {nombre: "Carro",      costo:21500},
+    {nombre: "Celular",    costo:5500},
+    {nombre: "Camara",     costo:6500},
+    {nombre: "Gorra",      costo:500},
+];
+
